8cf38a04967fcc34a3b5ab65815a3b64dd501194
Yvar-School <yvar.nanlohij001@fclive.nl>
2020-04-14 10:08:18 +0200

    yes

--- game.js
+++ game.js
@@ -1,41 +1,70 @@
 var paddle, paddleY;
 
+var gamearea = document.getElementById('gamearea');
+x = 0;
+y = 60;
+rY= 10;
+rX = 4;
 
 
-function init(){
+function init() {
     paddle = document.getElementById('paddle');
     stylepaddle = getComputedStyle(paddle);
     paddleY = parseInt(stylepaddle.top);
-    setInterval(run, 10);        
+    ball = document.getElementById('ball')
+    StyleBall = getComputedStyle(ball);
+    setInterval(run, 10);
 }
 
 function run() {
+    console.log(gamearea.clientWidth);
+    x += rX;
+    ball.style.left = x + "px";
+    y += rY;
+    ball.style.top = y + "px";
 
+    if ( (y + ball.clientHeight > gamearea.clientHeight + 44) || ( -50 + y < 0))
+    {
+        rY *= -1;
+    }
+    if (x + ball.clientWidth > gamearea.clientWidth)
+    {
+        rX *= -1.5;
+        ball.style.backgroundColor = getRandomColor();
+    }
+    
 }
 
 
-function upArrow(){
+function upArrow() {
     var element = document.getElementById('paddle');
     paddleY -= 10;
     element.style.top = paddleY + 'px';
-    console.log('up')
 }
 
-function downArrow(){
+function downArrow() {
     var element = document.getElementById('paddle');
     paddleY += 10;
     element.style.top = paddleY + 'px';
-    console.log('down')
 }
 
-document.onkeydown = function(event) {
-    switch(event.keyCode) {
+document.onkeydown = function (event) {
+    switch (event.keyCode) {
         case 38:
             upArrow();
-        break;
+            break;
         case 40:
             downArrow();
-        break;
+            break;
     }
 
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
